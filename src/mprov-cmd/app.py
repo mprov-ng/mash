bbc4b5350cd172d280606ad4fd7936b4e7597b42
Jason Williams <jasonw@jhu.edu>
2022-09-15 10:07:06 -0400

    more initial testing with python cmd

--- src/mprov-cmd/app.py
+++ src/mprov-cmd/app.py
@@ -4,6 +4,8 @@ class MprovShell(cmd.Cmd):
   intro = "Welcome to the mProv shell.  Type help or ? to list commands.\n"
   prompt = '<mProv> # '
   file = None
+  def setFile(self, file):
+    self.file = file
   def do_connect(self,arg):
     'Connect to an mProv Control Center. Args: <mPCC URL> <( api <api-key> ) | ( user <username> pass <password> )> '
     pass
@@ -30,6 +32,9 @@ class MprovShell(cmd.Cmd):
   def do_quit(self, arg):
     'Quit the Shell.'
     sys.exit(0)
+  def do_EOF(self, arg):
+    'Quit the Shell.'
+    sys.exit(0)
     
   # alias the http methods to their CRUD equivs.
   def do_post(self, arg):
@@ -47,6 +52,24 @@ class MprovShell(cmd.Cmd):
   # pre-process commandline if needed
   def precmd(self, line):
     return line
+
+  def cmdloop(self, intro=None):
+    if self.file != None:
+      # we are getting a file piped in.
+      # it should be an FD, not a file name.
+      for line in self.file:
+
+        line=line.strip()
+        # skip comments.
+        if line[0] == "#": 
+          continue
+        
+        self.onecmd(line)
+    else:
+      return super().cmdloop(intro=intro)
+
+
+
 
 
 
--- src/mprov-cmd/main.py
+++ src/mprov-cmd/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-import signal
+import signal, os, sys
+from stat import S_ISFIFO
 from app import MprovShell
 
 # an exit handler if we need it.
@@ -13,7 +14,22 @@ signal.signal(signal.SIGTERM, exitHandler)
 
 
 def main():
-  MprovShell().cmdloop()
+  
+  shell = MprovShell()
+  
+  # we are getting a script piped to us.
+  if S_ISFIFO(os.fstat(0).st_mode):
+    shell.setFile(sys.stdin)
+  else:
+    if os.path.exists(sys.argv[1]):
+      # we are getting a file passed to us.
+      shell.setFile(open(sys.argv[1], 'r'))
+      if shell.file == None:
+        print(f"Error: Unable to open {sys.argv[1]}")
+        sys.exit(1)
+    else:
+      shell.setFile(None)
+  shell.cmdloop()
 
 def __main__():
   main()
